86570e654b24a26d7c399d2f7dad83e9b35b1e09
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-04-18 09:54:30 +0200

    Odd averager cleaned up from useless lines.

--- oddavg/odd_avg.py
+++ oddavg/odd_avg.py
@@ -1,7 +1,7 @@
 # Create a function called `odd_average` that takes a list of numbers as parameter
 # and returns the average value of the odd numbers in the list
 # Create basic unit tests for it with at least 3 different test cases
-#alma = [3, 6, 7 , 9] 6.3
+
 class Odd_Averager(object):
     def odd_average(self, numbers = [0]):
         summarize = 0
