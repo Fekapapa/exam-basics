bea5b5516e55e2eec3852000b64ec79e9be7792d
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-04-18 10:42:53 +0200

    Copy exercise. Class ready, controller, reader, and writer waits for test.

--- copy/copy.py
+++ copy/copy.py
@@ -6,3 +6,36 @@
 # No destination provided
 # When both arguments provided and the source is a file
 # Read all contents from it and write it to the destination
+import sys
+
+class Copy(object):
+    def __init__(self):
+        self.controller()
+
+    def controller(self):
+        if len(sys.argv) == 1:
+            print("copy [source] [destination]")
+        if len(sys.argv) == 2:
+            print("No destination provided")
+        if len(sys.argv) == 3:
+            self.destination = sys.argv[2]
+            self.data_reader(sys.argv[1])
+
+    def data_reader(self, source):
+        while True:
+            try:
+                self.raw_data = open(source, "r")
+                self.data = self.raw_data.read()
+                self.raw_data.close()
+                self.data_writer(self.data)
+                break
+            except:
+                print("Couldn't open source file.")
+                break
+                
+    def data_writer(self, meta):
+        self.data_write = open(self.destination, "w")
+        self.data_write.write(meta)
+        self.data_write.close()
+
+start = Copy()
