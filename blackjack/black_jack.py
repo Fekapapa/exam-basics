28b0134cbdc1d9626233b018310f2c1f6f127021
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-04-18 12:48:58 +0200

    Reordering init and new. Try to learn how new works.

--- blackjack/black_jack.py
+++ blackjack/black_jack.py
@@ -17,8 +17,6 @@ class Card(object):
         self.color = ["Clubs", "Diamonds", "Hearts", "Spades"]
 
 class Deck(object):
-    def __init__(self, number=0):
-        self.number = number
 
     def __new__(self, number=0):
         self.number = number
@@ -27,6 +25,9 @@ class Deck(object):
         + ", " + str(self.number // 4) + " " + str(Card().color[2])
         + ", " + str(self.number // 4) + " " + str(Card().color[3]))
         return self.deck_print
+
+    def __init__(self, number=0):
+        self.number = number
 
 #    def draw(self):
 #        self.deck_print = str(self.number) + " cards -  " + str(self.number // 4) + " " + str(Card().color[0]) \
